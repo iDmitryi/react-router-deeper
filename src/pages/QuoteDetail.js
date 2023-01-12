d22ab8295ec29d4cb49c9e01de828b7a4b03ad36
Dumitru Hanganu <dumitru.hanganu@ivfuture.uk>
2023-01-12 08:08:01 +0200

    init

---
+++ src/pages/QuoteDetail.js
@@ -0,0 +1,64 @@
+import React, { useEffect } from "react";
+import { Route, useParams, Link, useRouteMatch } from "react-router-dom";
+
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+import Comments from "../components/comments/Comments";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+
+import useHttp from "../hooks/use-http";
+import { getSingleQuote } from "../lib/api";
+
+const QuoteDetail = () => {
+  const match = useRouteMatch();
+  const param = useParams();
+
+  const { quoteId } = param;
+
+  const {
+    sendRequest,
+    status,
+    data: loadedQuote,
+    error,
+  } = useHttp(getSingleQuote, true);
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  //   const quote = DUMMY_QUOTES.find((quote) => quote.id === param.quoteId);
+
+  if (status === "pending") {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className="centered">{error}</p>;
+  }
+
+  if (!loadedQuote.text) {
+    return <p>No Quote Found</p>;
+  }
+
+  return (
+    <>
+      <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
+      <Route path={match.path} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`${match.url}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
+
+      <Route path={`${match.path}/comments`}>
+        <Comments />
+      </Route>
+    </>
+  );
+};
+
+export default QuoteDetail;
